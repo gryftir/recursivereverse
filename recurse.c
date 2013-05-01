8a4613c0797066dc711dce55480073f9adf3d5a1
Lawrence Siebert <gryftir@gmail.com>
2013-05-01 12:23:13 -0700

    added tail recursion, and interative version of tail recursion

--- recurse.c
+++ recurse.c
@@ -10,25 +10,70 @@ typedef struct NODE
 
 }NODE;
 
+
+
+// function declarations
 NODE * makenode (int i);
 NODE * pushNode (NODE * list, int i);
-
 NODE * recursiveReverse(NODE * current);
 void printList (NODE * first);
+NODE * tailrecursereverse(NODE * current, NODE * next);
+
+NODE * interativetail(NODE * current);
+
 int main (int argc, char * argv[]){
 int count;
 NODE * list = makenode(0);
 for (count = 1; count <= 10; count++)
 	list = pushNode(list,count);
 
+
+
 printf("before reverse\n");
 printList(list);
 list = recursiveReverse(list);
 printf ("after reverse\n");
 printList(list);		
+printf("now tailrecursereverse\n");
+list = tailrecursereverse(list, NULL);
+printList(list);
+printf("interative reversal based on tail reversal\n");
+list = interativetail(list);
+printList(list);
+
 
 	return 0;
 }
+
+
+NODE * tailrecursereverse(NODE * current, NODE * append){
+	NODE * next;
+	if (current == append)
+		return current;
+	if (current->next == NULL) {
+		current->next = append;
+		return current;
+	}
+	next = current->next;
+	current->next = append;
+	return tailrecursereverse(next, current);
+}
+
+NODE * interativetail(NODE * current){
+	NODE * next;
+	NODE * append = NULL;
+	if (current == append)
+		return current;
+	while (current->next != NULL) {
+		next = current->next;
+		current->next = append;
+		append = current;
+    current = next;
+	}
+	current->next = append;
+		return current;
+}
+
 
 NODE * makenode (int i){
 	NODE * node;
